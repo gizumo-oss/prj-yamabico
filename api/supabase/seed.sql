35769329d14b402e43358322f159016477966879
Kiyoshi Sakata <sakata@gizumo-inc.jp>
2024-07-16 20:13:10 +0900

    refactor: 可読性の向上

--- api/supabase/seed.sql
+++ api/supabase/seed.sql
@@ -20,14 +20,34 @@ SET row_security = off;
 -- Data for Name: users; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
 --
 
-INSERT INTO auth.users (instance_id, id, aud, role, email, encrypted_password, email_confirmed_at, invited_at, confirmation_token, confirmation_sent_at, recovery_token, recovery_sent_at, email_change_token_new, email_change, email_change_sent_at, last_sign_in_at, raw_app_meta_data, raw_user_meta_data, is_super_admin, created_at, updated_at, phone, phone_confirmed_at, phone_change, phone_change_token, phone_change_sent_at, email_change_token_current, email_change_confirm_status, banned_until, reauthentication_token, reauthentication_sent_at, is_sso_user, deleted_at, is_anonymous) VALUES ('00000000-0000-0000-0000-000000000000', 'e5e4e782-a453-4651-bf30-1d00721fe9bf', 'authenticated', 'authenticated', 'test@example.com', '$2a$10$XIUiXnxD2UG/l8AzlC8Lc.OsPEPbTTreWR16t7Gd9zFuNEwOqbEMy', '2024-07-02 12:10:04.674221+00', NULL, '', NULL, '', NULL, '', '', NULL, NULL, '{"provider": "email", "providers": ["email"]}', '{}', NULL, '2024-07-02 12:10:04.666569+00', '2024-07-02 12:10:04.67443+00', NULL, NULL, '', '', NULL, '', 0, NULL, '', NULL, false, NULL, false);
+INSERT INTO auth.users (
+    instance_id, id, aud, role, email,
+    encrypted_password, email_confirmed_at, invited_at, confirmation_token, confirmation_sent_at,
+    recovery_token, recovery_sent_at, email_change_token_new, email_change, email_change_sent_at,
+    last_sign_in_at, raw_app_meta_data, raw_user_meta_data, is_super_admin, created_at, updated_at,
+    phone, phone_confirmed_at, phone_change, phone_change_token, phone_change_sent_at,
+    email_change_token_current, email_change_confirm_status, banned_until, reauthentication_token, reauthentication_sent_at,
+    is_sso_user, deleted_at, is_anonymous)
+VALUES
+    ('00000000-0000-0000-0000-000000000000', 'e5e4e782-a453-4651-bf30-1d00721fe9bf', 'authenticated', 'authenticated', 'test@example.com',
+    '$2a$10$XIUiXnxD2UG/l8AzlC8Lc.OsPEPbTTreWR16t7Gd9zFuNEwOqbEMy', '2024-07-02 12:10:04.674221+00', NULL, '', NULL,
+    '', NULL, '', '', NULL,
+    NULL, '{"provider": "email", "providers": ["email"]}', '{}', NULL, '2024-07-02 12:10:04.666569+00', '2024-07-02 12:10:04.67443+00',
+    NULL, NULL, '', '', NULL,
+    '', 0, NULL, '', NULL,
+    false, NULL, false);
 
 
 --
 -- Data for Name: identities; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
 --
 
-INSERT INTO auth.identities (provider_id, user_id, identity_data, provider, last_sign_in_at, created_at, updated_at, id) VALUES ('e5e4e782-a453-4651-bf30-1d00721fe9bf', 'e5e4e782-a453-4651-bf30-1d00721fe9bf', '{"sub": "e5e4e782-a453-4651-bf30-1d00721fe9bf", "email": "test@example.com", "email_verified": false, "phone_verified": false}', 'email', '2024-07-02 12:10:04.671115+00', '2024-07-02 12:10:04.671167+00', '2024-07-02 12:10:04.671167+00', '56dc7097-2fd3-447d-81cc-0c7ba136746e');
+INSERT INTO auth.identities (
+    provider_id, user_id, identity_data, provider, last_sign_in_at,
+    created_at, updated_at, id)
+VALUES
+    ('e5e4e782-a453-4651-bf30-1d00721fe9bf', 'e5e4e782-a453-4651-bf30-1d00721fe9bf', '{"sub": "e5e4e782-a453-4651-bf30-1d00721fe9bf", "email": "test@example.com", "email_verified": false, "phone_verified": false}', 'email', '2024-07-02 12:10:04.671115+00',
+    '2024-07-02 12:10:04.671167+00', '2024-07-02 12:10:04.671167+00', '56dc7097-2fd3-447d-81cc-0c7ba136746e');
 
 
 --
